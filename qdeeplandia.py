c3eebec79e1a83120f6ae1579334485122cf527d
Raphaël Delhome <raphael.delhome@oslandia.com>
2020-04-01 12:54:06 +0200

    packaging: init the plugin main module

---
+++ qdeeplandia.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+#
+#   Copyright (C) 2020 Oslandia <infos@oslandia.com>
+#
+#   This file is a piece of free software; you can redistribute it and/or
+#   modify it under the terms of the GNU Library General Public
+#   License as published by the Free Software Foundation; either
+#   version 2 of the License, or (at your option) any later version.
+#
+#   This library is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+#   Library General Public License for more details.
+#   You should have received a copy of the GNU Library General Public
+#   License along with this library; if not, see <http://www.gnu.org/licenses/>.
+#
+
+
+class QDeeplandiaPlugin:
+    def __init__(self, iface):
+        self.iface = iface
+
+    def initGui(self):
+        pass
+
+    def unload(self):
+        pass
+
+    def load_trained_model(self):
+        pass
+
+    def infer(self):
+        pass

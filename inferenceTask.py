0b87259b3c34ffff1c75732c2054a904a815e048
speillet <sebastien.peillet@oslandia.com>
2020-05-22 10:38:43 +0200

    better method to get tmp dir

--- inferenceTask.py
+++ inferenceTask.py
@@ -19,12 +19,8 @@ class InferenceTask(QgsTask):
     def __init__(self, description, iface, layer, nb_label, model_path, extent=None):
         super().__init__(description, QgsTask.CanCancel)
         self.feedback = Feedback(iface)
-        if sys.platform == 'windows' :
-            tmp_folder = os.path.join(os.environ['LOCALAPPDATA'], 'QGIS')
-        else :
-            tmp_folder = '/tmp'
-        tmp_name = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase + string.digits) for _ in range(16)) + '.tif'
-        self.param = { 'INPUT' : layer.id(), 'OUTPUT' : os.path.join(tmp_folder,tmp_name), 'LABELS' : nb_label, 'MODEL' : model_path }
+        tmp_name = processing.getTempFilename() + '.tif'
+        self.param = { 'INPUT' : layer.id(), 'OUTPUT' : os.path.join(tmp_name), 'LABELS' : nb_label, 'MODEL' : model_path }
         if extent :
             self.param['EXTENT'] = extent
 
